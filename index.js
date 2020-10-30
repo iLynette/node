feef99d495a6e6c720c6e1609c6d39c413cb6b98
iLynette <lynetteacholah@icloud.com>
2020-10-30 08:21:56 +0300

    Date and time

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const person = require('./person');
+// const person = require('./person');
 
-console.log(person);
+// console.log(person);
+
+var http = require('http');
+var dt = require('./person');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('The date and time are currently: ' + dt.myDateTime());
+  res.end();
+  // res.end('Hello there node!');
+}).listen(3030);
--- person.js
+++ person.js
@@ -1,5 +1,10 @@
-const person = {
-    name: 'Lynette Acholah',
-    sex: 'female'
-}
-module.exports = person;
+// const person = {
+//     name: 'Lynette Acholah',
+//     sex: 'female'
+// }
+// module.exports = person;
+
+
+exports.myDateTime = function () {
+    return Date();
+};
